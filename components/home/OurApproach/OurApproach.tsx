55a46b45876b02749e4e016c79a4987915807b73
slv-dvishwakarma <dvishwakarma@skilline.ai>
2024-07-12 22:25:52 +0530

    images -changed

--- components/home/OurApproach/OurApproach.tsx
+++ components/home/OurApproach/OurApproach.tsx
@@ -6,7 +6,7 @@ export const OurApproach = () => {
     <div className="md:pb-[100px] md:mt-[30px]">
       <ImageBox
         data={{
-          image: "/image/images/our-approach.jpeg",
+          image: "/image/images/our_approach.jpg",
           title: "Our Approach",
           description:
             "With the help of AI and gamification, we offer a unique learning journey personalized to each learner's desire and need. Our platform provides high-quality education in multiple Indian languages, considering regional cultures. ",
