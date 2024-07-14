1689709004950a80e7d1c0e9c8342e6cebc9dbd1
slv-dvishwakarma <dvishwakarma@skilline.ai>
2024-07-14 17:23:59 +0530

    set spaceing

--- components/home/GuidingPrinciple/GuidingPrinciple.tsx
+++ components/home/GuidingPrinciple/GuidingPrinciple.tsx
@@ -12,9 +12,9 @@ export const GuidingPrinciple = () => {
   };
 
   return (
-    <div className="bg-gray-100 relative py-5">
-      <div className="container ">
-        <div className="sm:grid grid-cols-2 gap-10 items-center">
+    <div className="bg-gray-100 py-5">
+      <div className="container "> 
+        <div className="sm:grid grid-cols-2 gap-10 items-center md:space-y-0 space-y-5">
           <div className="grid-item col-span-1 relative space-y-5">
             <Heading>{data.title}</Heading>
             <Paragraph className=" font-light  tracking-[_0.4px]">
@@ -24,9 +24,9 @@ export const GuidingPrinciple = () => {
               <Link href="/how-it-works">Read More</Link>
             </Button>
           </div>
-          <div className="grid-item col-span-1 relative">
+          <div className="grid-item col-span-1">
             <Image
-              className="float-left shadow-[0px_0px_20px_0px_rgba(51,51,51,0.1)] rounded-[10px_10px_10px_10px] relative md:h-[480px] object-cover"
+              className="shadow-[0px_0px_20px_0px_rgba(51,51,51,0.1)] rounded-[10px_10px_10px_10px] md:h-[480px] md:object-cover"
               // src={data.image}
               src={data.image}
               alt={data.title}
